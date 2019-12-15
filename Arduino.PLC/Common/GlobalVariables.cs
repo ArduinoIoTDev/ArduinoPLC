0eca6eb5b113559a782994eb7e9188d07d9b07d5
ArduinoIoTDev <58135452+ArduinoIoTDev@users.noreply.github.com>
2019-12-15 11:00:43 +0900

    Add files via upload

---
+++ Arduino.PLC/Common/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class GlobalVariables
+{
+    public static string PLCType = "LSIS";
+
+    public static bool MONITORING_MODE = false;
+    public static Dictionary<string, string> GLOVAL_VARIABlE_LIST = new Dictionary<string, string>();
+}
